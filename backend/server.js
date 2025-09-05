34004c0344238edd3bc6220867c0fdb6283eff5b
sahilchawade04 <sahilchawade04@gmail.com>
2025-09-05 12:14:32 +0530

    change in frontend URL

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(express.urlencoded({extended:true}));
 
 app.use(cors({
-  origin: [process.env.CLIENT_URL, "http://localhost:3000"],
+  origin: [process.env.CLIENT_URL, "https://mern-blog-mjsf.vercel.app"],
   credentials: true
 }));
 
